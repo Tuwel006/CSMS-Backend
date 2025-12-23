64b6d3f7f3da520317623e42e70761799104b19c
Tuwel Shaikh <tuwelshaikh006@gmail.com>
2025-12-24 01:07:52 +0530

    fix: Update database connection settings in .env and db.ts for Railway compatibility

--- src/config/db.ts
+++ src/config/db.ts
@@ -2,7 +2,6 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import * as dotenv from 'dotenv';
 import { User } from '../modules/v1/shared/entities/User';
-
 dotenv.config();
 
 export const AppDataSource = new DataSource({
@@ -12,11 +11,12 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
-    synchronize: true, // Auto-sync in development
+    synchronize: true,
     logging: false,
-    entities: [__dirname+"/../modules/v1/shared/entities/*.{ts,js}"],
-    migrations: [],
-    subscribers: [],
+    entities: [__dirname + '/../modules/v1/shared/entities/*.{ts,js}'],
+    ssl: {
+        rejectUnauthorized: false, // required for Railway
+    },
 });
 
 export const connectDB = async () => {
